6de3df7717b2548c20b8307c2db6d514e03624d6
geunyang <gaeballza@gmail.com>
2022-06-21 15:35:56 +0900

    220621

--- 오라클/bookuser(2022-06-16).sql
+++ 오라클/bookuser(2022-06-16).sql
@@ -5,8 +5,15 @@ CREATE TABLE tbl_images(
     i_imageName VARCHAR2(255)
 );
 
+CREATE SEQUENCE seq_bbs
+INCREMENT by 1 START WITH 1;
+
 CREATE SEQUENCE seq_image
 INCREMENT by 1 START WITH 1;
 
 SELECT *FROM tbl_bbs, tbl_images
 WHERE b_seq = i_bseq;
+
+SELECT * FROM seq_image;
+
+commit;
